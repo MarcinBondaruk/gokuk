0bd7190e8377dd7c06dc359ab4eabf1cfcdc1413
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-02-02 00:01:35 +0100

    posgres

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/MarcinBondaruk/gokuk/configs"
 	"github.com/MarcinBondaruk/gokuk/internal/application/controller"
 	"github.com/MarcinBondaruk/gokuk/internal/application/router"
 )
 
 func main() {
+	postgres := configs.GetPostgresConnection()
+	defer configs.ClosePostgresConnection(postgres)
+
 	controller := controller.NewController()
 	router := router.NewRouter(controller)
 
--- configs/postgres.go
+++ configs/postgres.go
@@ -16,7 +16,7 @@ const (
 	dbname   = "gokuk"
 )
 
-func GetConnection() *gorm.DB {
+func GetPostgresConnection() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
 	db, err := gorm.Open(postgres.Open(dsn))
 
@@ -25,4 +25,18 @@ func GetConnection() *gorm.DB {
 	}
 
 	return db
+}
+
+func ClosePostgresConnection(db *gorm.DB) {
+	if db != nil {
+		sqlDB, err := db.DB()
+		if err != nil {
+			log.Fatal("failed to get database object. err:", err)
+		}
+
+		err = sqlDB.Close()
+		if err != nil {
+			log.Fatal("failed to close database connection. err:", err)
+		}
+	}
 }
