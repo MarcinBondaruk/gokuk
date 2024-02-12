54c1e4a96e2f51959485873ef8034a3d084a4b0a
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-02-12 20:17:40 +0100

    meal plan tables

--- migrations/20240211213931_init.up.sql
+++ migrations/20240211213931_init.up.sql
@@ -1,3 +1,9 @@
+CREATE TABLE users (
+    id uuid PRIMARY KEY,
+    username VARCHAR(64),
+    password VARCHAHR(64)
+);
+
 CREATE TABLE products (
     id uuid PRIMARY KEY,
     name VARCHAR(32) NOT NULL
@@ -6,7 +12,8 @@ CREATE TABLE products (
 CREATE TABLE recipes (
     id uuid PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    author_id REFERENCES users(id)
 );
 
 CREATE TABLE recipe_ingredient (
@@ -15,4 +22,15 @@ CREATE TABLE recipe_ingredient (
     recipe_id REFERENCES recipes(id) ON DELETE CASCADE,
     quantity INT NOT NULL,
     unit VARCHAR(2) NOT NULL
+);
+
+CREATE TABLE meal_plan (
+    id uuid PRIMARY KEY,
+    owner_id REFERENCES users(id)
+);
+
+CREATE TABLE meal_plan_recipe (
+    id uuid PRIMARY KEY,
+    meal_plan_id REFERENCES meal_plan(id) ON DELETE CASCADE
+    recipe_id REFERENCES recipes(id)
 );
