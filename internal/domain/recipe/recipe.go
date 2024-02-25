7cbdb566e70eb106d7d07837708bd62d40909826
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-02-25 21:47:47 +0100

    recipe

--- internal/domain/recipe/recipe.go
+++ internal/domain/recipe/recipe.go
@@ -1,37 +1,33 @@
 package recipe
 
 import (
-	"unicode/utf8"
-
 	"github.com/google/uuid"
 )
 
 type Recipe struct {
 	id          uuid.UUID
+	authorId    uuid.UUID
 	title       string
 	description string
 	ingredients []Ingredient
-	authorId    uuid.UUID
 }
 
-func NewRecipe(
-	id uuid.UUID,
-	title string,
-	description string,
-	authorId uuid.UUID,
-) (*Recipe, error) {
-	if utf8.RuneCountInString(title) > 128 {
-		return nil, NewInvalidTitleLengthErr()
-	}
+func (r *Recipe) Id() string {
+	return r.id.String()
+}
 
-	if utf8.RuneCountInString(description) > 5000 {
-		return nil, NewInvalidDescriptionLengthErr()
-	}
+func (r *Recipe) AuthorId() string {
+	return r.authorId.String()
+}
 
-	return &Recipe{
-		id:          id,
-		title:       title,
-		description: description,
-		authorId:    authorId,
-	}, nil
+func (r *Recipe) Title() string {
+	return r.title
+}
+
+func (r *Recipe) Description() string {
+	return r.description
+}
+
+func (r *Recipe) Ingredients() []Ingredient {
+	return r.ingredients
 }
