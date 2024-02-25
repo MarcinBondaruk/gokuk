3faaeeae2ff0744924a36bbc6aabe3ba6b96d014
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2024-02-25 22:04:29 +0100

    menu

--- internal/domain/menu/menu.go
+++ internal/domain/menu/menu.go
@@ -1,3 +1,25 @@
 package menu
 
-type Menu struct{}
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Menu struct {
+	id      uuid.UUID
+	recipes []uuid.UUID
+	date    time.Time
+}
+
+func (m *Menu) Id() string {
+	return m.id.String()
+}
+
+func (m *Menu) Recipes() []uuid.UUID {
+	return m.recipes
+}
+
+func (m *Menu) Date() time.Time {
+	return m.date
+}
